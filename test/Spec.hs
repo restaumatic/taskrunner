540a3b8ce9d0c1ebdd8af43f94f6d555ea3425d4
Maciej Bielecki <zyla@prati.pl>
2024-08-02 06:23:49 +0000

    Test runner

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,11 @@ import Universum
 import Test.Tasty (defaultMain, TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsString, findByExtension)
 import qualified Data.ByteString.Lazy as LBS
-import System.FilePath (takeBaseName, replaceExtension)
-import Data.Char (toUpper)
-import qualified Data.ByteString as BS
+import System.FilePath (takeBaseName, replaceExtension, (</>))
+import System.Process (proc, createPipe, CreateProcess (..), StdStream (..), withCreateProcess, waitForProcess)
+import System.IO
+import System.IO.Temp (withSystemTempDirectory)
+import System.Exit (ExitCode(..))
 
 main :: IO ()
 main = defaultMain =<< goldenTests
@@ -17,12 +19,40 @@ goldenTests = do
     [ goldenVsString
         (takeBaseName inputFile) -- test name
         outputFile -- golden file path
-        (LBS.readFile inputFile >>= runTest) -- action whose result is tested
+        (System.IO.readFile inputFile >>= runTest) -- action whose result is tested
     | inputFile <- inputFiles
     , let outputFile = replaceExtension inputFile ".out"
     ]
 
-runTest :: LBS.ByteString -> IO LBS.ByteString
-runTest s = do
-  pure ""
-  -- TODO: create log tmpdir, call process, get output, get logfiles, delete log dir
+runTest :: String -> IO LBS.ByteString
+runTest source = do
+  withSystemTempDirectory "testrunner-test" \dir -> do
+    -- TODO: call process, get output, get logfiles
+
+    let logDir = dir </> "logs"
+
+    (pipeRead, pipeWrite) <- createPipe
+    withCreateProcess
+      (proc "taskrunner"
+        [ "sh"
+        , "-c"
+        , source
+        ]) { std_out = UseHandle pipeWrite, std_err = UseHandle pipeWrite
+        , env = Just
+          [ ("TASKRUNNER_LOG_DIRECTORY", logDir)
+          , ("TASKRUNNER_DISABLE_TIMESTAMPS", "1")
+          ]
+        } \_ _ _ processHandle -> do
+
+      output <- LBS.hGetContents pipeRead
+      -- FIXME: we can probably get a deadlock if the pipe is filled (since we're not reading from it yet)
+
+      exitCode <- waitForProcess processHandle
+
+      pure $ mconcat
+        [ "-- output:\n"
+        , output
+        , case exitCode of
+            ExitSuccess -> ""
+            ExitFailure code -> "-- exit code: " <> show code <> "\n"
+        ]

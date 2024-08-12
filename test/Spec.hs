3fd80173fb6ee03f43273cac7c76d3b9e26f9fc9
Maciej Bielecki <zyla@prati.pl>
2024-08-12 15:18:10 +0000

    log-output-parallel test

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,6 @@ import System.IO
 import System.IO.Temp (withSystemTempDirectory)
 import System.Exit (ExitCode(..))
 import System.Environment (getEnv)
-import Data.Text qualified as Text
 import System.FilePath.Glob as Glob
 import System.FilePath qualified as FP
 
@@ -31,7 +30,6 @@ goldenTests = do
 runTest :: String -> IO LBS.ByteString
 runTest source = do
   withSystemTempDirectory "testrunner-test" \dir -> do
-    let checkFileGlob = getCheckFileGlob (toText source)
     let logDir = dir </> "logs"
 
     (pipeRead, pipeWrite) <- createPipe
@@ -44,43 +42,43 @@ runTest source = do
         , "-c"
         , source
         ]) { std_out = UseHandle pipeWrite, std_err = UseHandle pipeWrite
-        , env = Just
-          [ ("TASKRUNNER_LOG_DIRECTORY", logDir)
-          , ("TASKRUNNER_LOCK_DIRECTORY", dir </> "locks")
-          , ("TASKRUNNER_DISABLE_TIMESTAMPS", "1")
-          , ("PATH", path)
-          ]
-        , cwd = Just dir
-        } \_ _ _ processHandle -> do
+          , env = Just
+            [ ("TASKRUNNER_LOG_DIRECTORY", logDir)
+--            , ("TASKRUNNER_LOCK_DIRECTORY", dir </> "locks")
+            , ("TASKRUNNER_DISABLE_TIMESTAMPS", "1")
+            , ("PATH", path)
+            ]
+          , cwd = Just dir
+          } \_ _ _ processHandle -> do
 
       output <- LBS.hGetContents pipeRead
       -- FIXME: we can probably get a deadlock if the pipe is filled (since we're not reading from it yet)
 
       exitCode <- waitForProcess processHandle
 
-      checkFiles <-
-        case checkFileGlob of
-          Nothing ->
-            pure mempty
-          Just g -> do
-            files <- globDir1 (Glob.compile g) dir
+      let checkFileGlobs = fromMaybe ["output"] $ getCheckFileGlobs (toText source)
+      checkFiles <- 
+        forM checkFileGlobs \glob' ->
+          if glob' == "output" then
+            pure ["-- output:\n" <> output]
+          else do
+            files <- globDir1 (Glob.compile glob') dir
             forM files \file -> do
               content <- LBS.readFile file
               let relativeFilename = FP.makeRelative dir file
               pure $ "-- " <> encodeUtf8 relativeFilename <> ":\n" <> content
 
       pure $ mconcat
-        [ "-- output:\n"
-        , output
-        , mconcat checkFiles
+        [ mconcat $ mconcat checkFiles
         , case exitCode of
             ExitSuccess -> ""
             ExitFailure code -> "-- exit code: " <> show code <> "\n"
         ]
 
-getCheckFileGlob :: Text -> Maybe String
-getCheckFileGlob source =
+getCheckFileGlobs :: Text -> Maybe [String]
+getCheckFileGlobs source =
   case lines source of
-    firstLine:_ ->
-      toString <$> Text.stripPrefix "# check " firstLine
+    firstLine:_
+      | "#": "check":globs <- words firstLine ->
+        Just $ toString <$> globs
     _ -> Nothing

ea7950c0f505782c40600e692304a82cfe370ea4
Maciej Bielecki <zyla@prati.pl>
2024-08-14 13:32:36 +0000

    Faster json argument seralization

--- src/snapshot.sh
+++ src/snapshot.sh
@@ -1,18 +1,5 @@
 # Source code of the `snapshot` function, injected into the wrapped script
 
-args_to_json() {
-  json_array="["
-
-  for arg in "${@}"; do
-      json_array+=$(jq -Rn --arg str "$arg" '$str')
-      json_array+=","
-  done
-
-  json_array="${json_array%,}]"
-
-  echo "$json_array"
-}
-
-args_to_json snapshot "$@" >&${_taskrunner_request_pipe}
+jq -n -c '$ARGS.positional' --args -- snapshot "$@" >&${_taskrunner_request_pipe}
 read -u ${_taskrunner_response_pipe} reply
 eval "$reply"
